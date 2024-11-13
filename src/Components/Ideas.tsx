768c5e8b445fdb3c4c53e7c1d2b9108628bf1574
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-13 09:26:17 +0100

    Added BookIconSmall component and a new div element with a hover effect and a "Read more articles" text.

--- src/Components/Ideas.tsx
+++ src/Components/Ideas.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookIcon from './Icons/BookIcon'
+import BookIconSmall from './Icons/BookIconSmall'
 
 const Ideas = () => {
     return (
@@ -10,6 +11,11 @@ const Ideas = () => {
                     <BookIcon />
                     <p className='text-[#131313] text-[100px] font-geist font-bold leading-none'>AND INNOVATIONS</p>
                 </div>
+            </div>
+
+            <div className='w-[385px] h-[67px] rounded-[200px] border-[1px] border-[#262626] hover:scale-110 transition ease-in-out cursor-pointer flex flex-row space-x-1 items-center justify-center'>
+                <BookIconSmall />
+                <p className='text-[36px] text-[#262626] font-geist'>Read more articles</p>
 
             </div>
 
