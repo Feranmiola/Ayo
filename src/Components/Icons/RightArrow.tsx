a6954de5ed3138d533d60a3084071dc984399f94
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-13 09:34:32 +0100

    "Refactored Ideas component layout and added new elements"

---
+++ src/Components/Icons/RightArrow.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightArrow = () => {
+    return (
+        <svg width="31" height="13" viewBox="0 0 31 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M30.5 6.5L20.5 0.726497V12.2735L30.5 6.5ZM0.5 7.5H21.5V5.5H0.5V7.5Z" fill="#131313" />
+        </svg>
+
+    )
+}
+
+export default RightArrow
--- src/Components/Ideas.tsx
+++ src/Components/Ideas.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import BookIcon from './Icons/BookIcon'
 import BookIconSmall from './Icons/BookIconSmall'
+import RightArrow from './Icons/RightArrow'
 
 const Ideas = () => {
     return (
         <div className='h-[1377px] w-full bg-white flex flex-col items-center justify-between py-20'>
-            <div className='flex flex-col justify-between h-[200px]'>
-                <p className='text-[#131313] text-[100px] font-geist font-bold leading-none'>IDEAS, INSIGHTS,</p>
-                <div className='flex flex-row items-center space-x-1'>
-                    <BookIcon />
-                    <p className='text-[#131313] text-[100px] font-geist font-bold leading-none'>AND INNOVATIONS</p>
+            <div className='flex flex-col space-y-5'>
+                <div className='flex w-[1062px] flex-col space-y-5'>
+                    <div className='flex flex-col justify-between h-[200px]'>
+                        <p className='text-[#131313] text-[100px] font-geist font-bold leading-none'>IDEAS, INSIGHTS,</p>
+                        <div className='flex flex-row items-center space-x-1'>
+                            <BookIcon />
+                            <p className='text-[#131313] text-[100px] font-geist font-bold leading-none'>AND INNOVATIONS</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-row items-center space-x-2'>
+                        <p className='text-[#262626] text-2xl font-geist'>From personal experiences</p>
+                        <RightArrow />
+                        <p className='text-[#262626] text-2xl font-geist'>to industry observations</p>
+                    </div>
                 </div>
-            </div>
 
+            </div>
             <div className='w-[385px] h-[67px] rounded-[200px] border-[1px] border-[#262626] hover:scale-110 transition ease-in-out cursor-pointer flex flex-row space-x-1 items-center justify-center'>
                 <BookIconSmall />
                 <p className='text-[36px] text-[#262626] font-geist'>Read more articles</p>
