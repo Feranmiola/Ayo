c6d6567c1fe8850aa4d9af826ec54310a3c400eb
feranmiola <osunjuyigbeiyin@gmail.com>
2024-12-06 00:18:23 +0100

    SEO setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,69 @@
 /* eslint-disable */
 // @ts-nocheck
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import Image from "next/image"
-import { motion, useAnimation } from "framer-motion"
-import Entrepreneur from "@/Components/Entreprenure"
-import Mission from "@/Components/Mission"
-import Ideas from "@/Components/Ideas"
-import Footer from "@/Components/Footer"
-import { useSticky } from "@/Components/UseSticky"
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { motion, useAnimation } from "framer-motion";
+import Entrepreneur from "@/Components/Entreprenure";
+import Mission from "@/Components/Mission";
+import Ideas from "@/Components/Ideas";
+import Footer from "@/Components/Footer";
+import { useSticky } from "@/Components/UseSticky";
+import SEO from "@/Components/SEO";
 
 export default function Home() {
-  const [missionRef, isMissionSticky] = useSticky()
-  const [ideasRef, isIdeasSticky] = useSticky()
-  const [selectedTab, setSelectedTab] = useState(1)
-  const [isHovered, setIsHovered] = useState(false)
-  const [hoverColor, setHoverColor] = useState('#B4F405')
-  const [prevColor, setPrevColor] = useState('')
-  const controls = useAnimation()
-  const underlineControls = useAnimation()
+  const [missionRef, isMissionSticky] = useSticky();
+  const [ideasRef, isIdeasSticky] = useSticky();
+  const [selectedTab, setSelectedTab] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+  const [hoverColor, setHoverColor] = useState("#B4F405");
+  const [prevColor, setPrevColor] = useState("");
+  const controls = useAnimation();
+  const underlineControls = useAnimation();
 
-  const colors = ['#B4F405', '#47D1FF', '#F8B154']
+  const colors = ["#B4F405", "#47D1FF", "#F8B154"];
 
   useEffect(() => {
     if (isHovered && window.innerWidth > 640) {
-      controls.start({ y: -10, filter: 'grayscale(0)', transition: { duration: 0.3, ease: 'easeOut' } })
-      underlineControls.start({ width: '100%', transition: { duration: 0.3, ease: 'easeOut' } })
+      controls.start({
+        y: -10,
+        filter: "grayscale(0)",
+        transition: { duration: 0.3, ease: "easeOut" },
+      });
+      underlineControls.start({
+        width: "100%",
+        transition: { duration: 0.3, ease: "easeOut" },
+      });
     } else {
-      controls.start({ y: 0, filter: 'grayscale(1)', transition: { duration: 0.3, ease: 'easeOut' } })
-      underlineControls.start({ width: 0, transition: { duration: 0.3, ease: 'easeOut' } })
+      controls.start({
+        y: 0,
+        filter: "grayscale(1)",
+        transition: { duration: 0.3, ease: "easeOut" },
+      });
+      underlineControls.start({
+        width: 0,
+        transition: { duration: 0.3, ease: "easeOut" },
+      });
     }
-  }, [isHovered, controls, underlineControls])
+  }, [isHovered, controls, underlineControls]);
 
   const handleHover = () => {
     if (window.innerWidth > 640) {
-      setIsHovered(true)
-      let newColor
+      setIsHovered(true);
+      let newColor;
       do {
-        newColor = colors[Math.floor(Math.random() * colors.length)]
-      } while (newColor === prevColor)
-      setHoverColor(newColor)
-      setPrevColor(newColor)
+        newColor = colors[Math.floor(Math.random() * colors.length)];
+      } while (newColor === prevColor);
+      setHoverColor(newColor);
+      setPrevColor(newColor);
     }
-  }
+  };
 
   const fadeInVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1 }
-  }
+    visible: { opacity: 1 },
+  };
 
   useEffect(() => {
     // Ensure this only runs in the browser
@@ -58,36 +73,39 @@ export default function Home() {
 
     // Function to update scrollbar properties
     function updateScrollbar() {
-      const scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
-      const scrollbarHeight = (window.innerHeight / document.documentElement.scrollHeight) * window.innerHeight;
-      const scrollTop = scrollPercentage * (window.innerHeight - scrollbarHeight);
+      const scrollPercentage =
+        window.scrollY /
+        (document.documentElement.scrollHeight - window.innerHeight);
+      const scrollbarHeight =
+        (window.innerHeight / document.documentElement.scrollHeight) *
+        window.innerHeight;
+      const scrollTop =
+        scrollPercentage * (window.innerHeight - scrollbarHeight);
 
-      document.body.style.setProperty('--scroll-top', `${scrollTop}px`);
-      document.body.style.setProperty('--scrollbar-height', `${scrollbarHeight}px`);
+      document.body.style.setProperty("--scroll-top", `${scrollTop}px`);
+      document.body.style.setProperty(
+        "--scrollbar-height",
+        `${scrollbarHeight}px`
+      );
 
-      document.body.classList.add('is-scrolling');
-
+      document.body.classList.add("is-scrolling");
 
       clearTimeout(scrollTimer);
       scrollTimer = setTimeout(() => {
-        document.body.classList.remove('is-scrolling');
+        document.body.classList.remove("is-scrolling");
       }, 1000);
     }
 
-
-    window.addEventListener('scroll', updateScrollbar);
-    window.addEventListener('resize', updateScrollbar);
-
+    window.addEventListener("scroll", updateScrollbar);
+    window.addEventListener("resize", updateScrollbar);
 
     updateScrollbar();
 
-
     return () => {
-      window.removeEventListener('scroll', updateScrollbar);
-      window.removeEventListener('resize', updateScrollbar);
+      window.removeEventListener("scroll", updateScrollbar);
+      window.removeEventListener("resize", updateScrollbar);
     };
   }, []);
-
 
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
@@ -98,16 +116,25 @@ export default function Home() {
 
   return (
     <div className="relative w-screen flex flex-col items-center justify-center bg-darkGray">
-      <div className='hidden sm:flex w-[416px] h-[72px] rounded-[500px] items-center absolute z-[9999] top-10 bg-[#181818] justify-center flex-row -space-x-3'>
+      <SEO
+        title="AYO"
+        description=""
+        canonical="https://ayo-gamma.vercel.app/"
+        ogImage=""
+        ogType="website"
+        twitterHandle=""
+      />
+      <div className="hidden sm:flex w-[416px] h-[72px] rounded-[500px] items-center absolute z-[9999] top-10 bg-[#181818] justify-center flex-row -space-x-3">
         <div
           onClick={() => {
             setSelectedTab(1);
             scrollToSection("Ayo");
           }}
-          className={`flex items-center justify-center z-40 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${selectedTab === 1
-            ? 'bg-white border-transparent font-bold text-[#181818]'
-            : 'border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]'
-            }`}
+          className={`flex items-center justify-center z-40 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${
+            selectedTab === 1
+              ? "bg-white border-transparent font-bold text-[#181818]"
+              : "border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]"
+          }`}
         >
           Ayo
         </div>
@@ -116,10 +143,11 @@ export default function Home() {
             setSelectedTab(2);
             scrollToSection("Journey");
           }}
-          className={`flex items-center justify-center z-30 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${selectedTab === 2
-            ? 'bg-white border-transparent font-bold text-[#181818]'
-            : 'border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]'
-            }`}
+          className={`flex items-center justify-center z-30 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${
+            selectedTab === 2
+              ? "bg-white border-transparent font-bold text-[#181818]"
+              : "border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]"
+          }`}
         >
           My Journey
         </div>
@@ -128,10 +156,11 @@ export default function Home() {
             setSelectedTab(3);
             scrollToSection("blog");
           }}
-          className={`flex items-center justify-center z-20 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${selectedTab === 3
-            ? 'bg-white border-transparent font-bold text-[#181818]'
-            : 'border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]'
-            }`}
+          className={`flex items-center justify-center z-20 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${
+            selectedTab === 3
+              ? "bg-white border-transparent font-bold text-[#181818]"
+              : "border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]"
+          }`}
         >
           Blog
         </div>
@@ -140,10 +169,11 @@ export default function Home() {
             setSelectedTab(4);
             scrollToSection("contact");
           }}
-          className={`flex items-center justify-center z-10 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${selectedTab === 4
-            ? 'bg-white border-transparent font-bold text-[#181818]'
-            : 'border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]'
-            }`}
+          className={`flex items-center justify-center z-10 px-5 py-2 rounded-[32px] border-[1px] transition ease-in-out text-base ${
+            selectedTab === 4
+              ? "bg-white border-transparent font-bold text-[#181818]"
+              : "border-[#A8A8A8] cursor-pointer font-semibold bg-[#131313] hover:text-white hover:border-white text-[#A8A8A8]"
+          }`}
         >
           Contact
         </div>
@@ -155,8 +185,13 @@ export default function Home() {
         variants={fadeInVariants}
         transition={{ duration: 1, ease: "easeOut" }}
       >
-        <div id="Ayo" className="flex flex-col sm:flex-row min-h-screen items-center sm:space-x-10 justify-center py-[5rem] sm:py-[10rem]">
-          <p className="text-[80px] sm:text-[100px] md:text-[140px] 2xl:text-[181.52px] opacity-50 font-bricolage text-white font-bold mb-4 sm:mb-0">Hi, i'm</p>
+        <div
+          id="Ayo"
+          className="flex flex-col sm:flex-row min-h-screen items-center sm:space-x-10 justify-center py-[5rem] sm:py-[10rem]"
+        >
+          <p className="text-[80px] sm:text-[100px] md:text-[140px] 2xl:text-[181.52px] opacity-50 font-bricolage text-white font-bold mb-4 sm:mb-0">
+            Hi, i'm
+          </p>
           <div
             id="ahyoh"
             className="flex relative flex-col sm:flex-row items-center"
@@ -166,7 +201,7 @@ export default function Home() {
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: isHovered ? 1 : 0, y: isHovered ? 0 : 20 }}
-              transition={{ duration: 0.8, ease: 'easeOut' }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
               className="absolute hidden sm:flex items-center justify-center -top-10 right-0 sm:right-[16rem] h-[40px] w-[97px] rounded-[12px] bg-[#262626]"
               style={{ color: hoverColor }}
             >
@@ -194,7 +229,9 @@ export default function Home() {
                   objectFit="cover"
                   width={142.95}
                   height={142.95}
-                  className={`rounded-[27.23px] transition-all duration-800 border-[1px] ${isHovered ? 'sm:border-white' : 'border-transparent'}`}
+                  className={`rounded-[27.23px] transition-all duration-800 border-[1px] ${
+                    isHovered ? "sm:border-white" : "border-transparent"
+                  }`}
                 />
               </div>
             </motion.div>
@@ -203,8 +240,13 @@ export default function Home() {
               <div className="relative inline-block">
                 <motion.p
                   className="text-[80px] sm:text-[100px] md:text-[140px] 2xl:text-[181.52px] font-bricolage font-bold leading-none"
-                  animate={{ color: isHovered && window.innerWidth > 640 ? hoverColor : 'white' }}
-                  transition={{ duration: 0.8, ease: 'easeOut' }}
+                  animate={{
+                    color:
+                      isHovered && window.innerWidth > 640
+                        ? hoverColor
+                        : "white",
+                  }}
+                  transition={{ duration: 0.8, ease: "easeOut" }}
                 >
                   Ayo
                 </motion.p>
@@ -212,7 +254,7 @@ export default function Home() {
                   className="absolute bottom-[10px] left-0 h-[5px] w-full origin-left hidden sm:block"
                   initial={{ scaleX: 0 }}
                   animate={{ scaleX: isHovered ? 1 : 0 }}
-                  transition={{ duration: 0.5, ease: 'easeOut' }}
+                  transition={{ duration: 0.5, ease: "easeOut" }}
                   style={{ backgroundColor: hoverColor }}
                 />
               </div>
@@ -222,11 +264,21 @@ export default function Home() {
 
         <Entrepreneur />
 
-        <div id="Journey" ref={missionRef} className={`min-h-screen ${isMissionSticky ? 'sticky top-0 z-10' : ''}`}>
+        <div
+          id="Journey"
+          ref={missionRef}
+          className={`min-h-screen ${
+            isMissionSticky ? "sticky top-0 z-10" : ""
+          }`}
+        >
           <Mission />
         </div>
 
-        <div id="blog" ref={ideasRef} className={`min-h-screen ${isIdeasSticky ? 'sticky top-0 z-20' : ''}`}>
+        <div
+          id="blog"
+          ref={ideasRef}
+          className={`min-h-screen ${isIdeasSticky ? "sticky top-0 z-20" : ""}`}
+        >
           <Ideas />
         </div>
         <div id="contact">
@@ -234,6 +286,5 @@ export default function Home() {
         </div>
       </motion.div>
     </div>
-  )
+  );
 }
-
